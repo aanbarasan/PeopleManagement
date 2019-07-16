6f14f7535f43d673d65958d327515373a375b75a
Anbarasan <anbarasanmbbs@gmail.com>
2019-07-16 18:19:21 +0530

    image update

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -4,7 +4,7 @@ import { UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle, Nav, 
 import PropTypes from 'prop-types';
 
 import { AppAsideToggler, AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
-import logo from '../../assets/img/brand/logo.svg'
+import logo from '../../assets/img/brand/logo.png'
 import sygnet from '../../assets/img/brand/sygnet.svg'
 
 const propTypes = {
@@ -23,7 +23,7 @@ class DefaultHeader extends Component {
       <React.Fragment>
         <AppSidebarToggler className="d-lg-none" display="md" mobile />
         <AppNavbarBrand
-          full={{ src: logo, width: 89, height: 25, alt: 'People Management' }}
+          full={{ src: logo, width: 89, height: 35, alt: 'People Management' }}
           minimized={{ src: sygnet, width: 30, height: 30, alt: 'People Management' }}
         />
         <AppSidebarToggler className="d-md-down-none" display="lg" />
